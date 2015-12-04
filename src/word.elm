c85849a3619921ae85f39a5c2e68e4d4a943b8e3
Nazanin Delam <nazanin1427@gmail.com>
2015-12-03 22:32:56 -0800

    model setup

--- src/word.elm
+++ src/word.elm
@@ -1,4 +1,4 @@
-module Word (Model)
+module Word (Model, initialModel, update, view) where
 
 import Html exposing (div, button, text)
 import Html.Events exposing (onClick)
@@ -8,5 +8,23 @@ import StartApp.Simple exposing (start)
 
 type alias Model =
   {
-  size: Int
+  freq = Int,
+  value = String
 }
+
+initialModel: String -> Int -> Model
+initialModel val num =
+  { value = val,
+    freq = num
+  }
+
+view : Signal.Address Status -> Model -> Html.Html
+view address model =
+  div [Html.Attributes.class "wordWrapper"] [
+    Html.span [] [ text Model.value ]
+     ]
+  ]
+
+update: Status -> Model -> Model
+update action model =
+  model
--- src/wordCloud.elm
+++ src/wordCloud.elm
@@ -2,11 +2,10 @@ module WordCloud where
 
 import Html exposing (div, button, text, span, p, img)
 import Html.Events exposing (onClick)
-import Signal.Time exposing (..)
 import Html.Attributes
 import StartApp.Simple exposing (start)
 import List
-import Card
+import Word
 import Debug exposing (log)
 import String
 import Random
@@ -21,3 +20,25 @@ main =
 type alias Model = {
    words: List Word Model
 }
+
+init: Model
+init =
+  {
+    words = [Word.initialModel("Apple" 10)]
+  }
+
+
+view: Signal.Address Action -> Model -> Html.Html
+view address model =
+  div [Html.Attributes.class "cloudContainer"]
+        [
+          div [Html.Attributes.style  [("width", "300px")]]
+          (List.map (\cModel -> Word.view (Signal.forwardTo address (Do cModel.id)) cModel) model.words)
+        ]
+
+
+
+
+update: Action -> Model -> Model
+update action model =
+      model
